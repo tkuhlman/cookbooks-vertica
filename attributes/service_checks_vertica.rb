a5768559e6748956ba1485431fd654001213169a
Tim Kuhlman <tim.kuhlman@hp.com>
2013-06-05 16:25:21 -0600

    Added thresholds for Vertica.
    Change-Id: Ifda9446d80f44843f54efbe21daeab5910822003

--- attributes/service_checks_vertica.rb
+++ attributes/service_checks_vertica.rb
@@ -47,8 +47,56 @@ default[:icinga][:check_params][:spread_daemon] = {
   :command => "/usr/lib/nagios/plugins/check_procs -c 1: -C spread"
 }
 
+#Thresholds
+default[:icinga][:service_checks][:vertica_request_queue_depth] = {
+  :service_description => "vertica_request_queue_depth",
+  :use => "generic-passive-service",
+  :freshness_threshold => 900,
+  :servicegroups => ["SOM"],
+  :contact_groups => ["som_team"]
+}
+default[:icinga][:check_params][:vertica_request_queue_depth] = {
+  :user => "nagios",
+  :hostgroups => ["role[Vertica-Node]"],
+  :check_interval => 3, #For passive services this is minutes
+  :command => "/usr/bin/check_graphite -n yes -u http://graphite.#{node[:domain]}/render/?target=Monitoring.#{node[:fqdn].gsub('.', '_')}.vertica.vertica-request_queue_depth-gauge--value.~&from-5minutes&rawData=true -w 5 -c 10"
+}
+
+default[:icinga][:service_checks][:vertica_sessions] = {
+  :service_description => "vertica_sessions",
+  :use => "generic-passive-service",
+  :freshness_threshold => 900,
+  :servicegroups => ["SOM"],
+  :contact_groups => ["som_team"]
+}
+default[:icinga][:check_params][:vertica_sessions] = {
+  :user => "nagios",
+  :hostgroups => ["role[Vertica-Node]"],
+  :check_interval => 3, #For passive services this is minutes
+  # The nagios plugin doesn't fully escape so the \\ before an %2C is needed
+  :command => "/usr/bin/check_graphite -n yes -u http://graphite.#{node[:domain]}/render/?target=sumSeries(Monitoring.#{node[:fqdn].gsub('.', '_')}.vertica.vertica-active_system_session_count-gauge--value.~\\%2CMonitoring.#{node[:fqdn].gsub('.', '_')}.vertica.vertica-active_user_session_count-gauge--value.~)&from-5minutes&rawData=true -w 100 -c 150"
+}
+
+default[:icinga][:service_checks][:vertica_percent_disk_free] = {
+  :service_description => "vertica_percent_disk_free",
+  :use => "generic-passive-service",
+  :freshness_threshold => 7200,
+  :servicegroups => ["SOM"],
+  :contact_groups => ["som_team"]
+}
+default[:icinga][:check_params][:vertica_percent_disk_free] = {
+  :user => "nagios",
+  :hostgroups => ["role[Vertica-Node]"],
+  :check_interval => 60, #For passive services this is minutes
+  # The nagios plugin doesn't fully escape so the \\ before an %2C is needed
+  :command => "/usr/bin/check_graphite -n yes -u http://graphite.#{node[:domain]}/render/?target=scale(divideSeries(Monitoring.#{node[:fqdn].gsub('.', '_')}.vertica.vertica-disk_space_used_mb-gauge--value.~\\%2CsumSeries(Monitoring.#{node[:fqdn].gsub('.', '_')}.vertica.vertica-disk_space_used_mb-gauge--value.~\\%2CMonitoring.#{node[:fqdn].gsub('.', '_')}.vertica.vertica-disk_space_free_mb-gauge--value.~))\\%2C100)&from-60minutes&rawData=true -w 60 -c 75"
+}
+
 #Enable passive checks
 default[:icinga][:client][:passive_checks_enabled] = {
   "vertica_daemon" => true,
-  "spread_daemon" => true
+  "spread_daemon" => true,
+  "vertica_request_queue_depth" => true,
+  "vertica_sessions" => true,
+  "vertica_percent_disk_free" => true
 }
