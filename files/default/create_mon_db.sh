5386c4a0b014855b333b86327ffd7d9d64b6b56b
Roland Hochmuth <roland.hochmuth@hp.com>
2014-02-28 21:30:57 -0700

    Renamed some files.

--- files/default/create_mon_db.sh
+++ files/default/create_mon_db.sh
@@ -10,8 +10,11 @@ fi
 # create the db
 /opt/vertica/bin/admintools -t create_db -s 127.0.0.1 -d mon -p password
 
-# Add in the schema
+# Add in the schemas
 /opt/vertica/bin/vsql -w password < /var/vertica/mon_schema.sql
+/opt/vertica/bin/vsql -w password < /var/vertica/mon_metrics_schema.sql
+/opt/vertica/bin/vsql -w password < /var/vertica/mon_users.sql
+/opt/vertica/bin/vsql -w password < /var/vertica/mon_grants.sql
 
 # For ssl support link the cert/key and restart the db
 ln /var/vertica/catalog/server* /var/vertica/catalog/mon/v*/
