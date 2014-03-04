35d0bfbe84fba8119b4109ab9972c7f021447ed2
Deklan Dieterly <deklan.dieterly@hp.com>
2014-03-04 12:13:33 -0700

    Update mon_metrics_schema.sql
    Need to store hash ids as binary data.
    
    dbadmin=> \d monmetrics.metrics;
                                                 List of Fields by Tables
       Schema   |  Table  |        Column        |    Type    | Size | Default | Not Null | Primary Key | Foreign Key 
    ------------+---------+----------------------+------------+------+---------+----------+-------------+-------------
     MonMetrics | Metrics | metric_id            | int        |    8 |         | t        | t           | 
     MonMetrics | Metrics | metric_definition_id | binary(20) |   20 |         | t        | f           | 
     MonMetrics | Metrics | time_stamp           | timestamp  |    8 |         | t        | f           | 
     MonMetrics | Metrics | value                | float      |    8 |         | t        | f           | 
    (4 rows)
    
    
    dbadmin=> select * from monmetrics.metrics;
     metric_id |                    metric_definition_id                     |     time_stamp      | value 
    -----------+-------------------------------------------------------------+---------------------+-------
             1 | \201x\362\261.\347\033\213$7\314f\033\211\372D\017\203\225i | 1973-11-26 00:52:03 |   123
             2 | \201x\362\261.\347\033\213$7\314f\033\211\372D\017\203\225i | 1973-11-26 00:52:03 |   123
    (2 rows)
    
    dbadmin=> select to_hex(metric_definition_id)  from monmetrics.metrics;
                      to_hex                  
    ------------------------------------------
     8178f2b12ee71b8b2437cc661b89fa440f839569
     8178f2b12ee71b8b2437cc661b89fa440f839569
    (2 rows)

--- files/default/mon_metrics_schema.sql
+++ files/default/mon_metrics_schema.sql
@@ -2,14 +2,14 @@ CREATE SCHEMA MonMetrics;
 
 CREATE TABLE MonMetrics.Metrics(
     metric_id AUTO_INCREMENT,
-    metric_definition_id VARCHAR(20) NOT NULL,
+    metric_definition_id BINARY(20) NOT NULL,
     time_stamp TIMESTAMP NOT NULL,
     value FLOAT NOT NULL,
     PRIMARY KEY(metric_id)
 ) PARTITION BY EXTRACT('year' FROM time_stamp)*10000 + EXTRACT('month' FROM time_stamp)*100 + EXTRACT('day' FROM time_stamp);
 
 CREATE TABLE MonMetrics.Definitions(
-    metric_definition_id VARCHAR(20) NOT NULL,
+    metric_definition_id BINARY(20) NOT NULL,
     name VARCHAR NOT NULL,
     tenant_id VARCHAR(14) NOT NULL,
     region VARCHAR NOT NULL,
@@ -18,21 +18,21 @@ CREATE TABLE MonMetrics.Definitions(
 );
 
 CREATE TABLE MonMetrics.StagedDefinitions(
-   metric_definition_id VARCHAR(20) NOT NULL,
+   metric_definition_id BINARY(20) NOT NULL,
    name VARCHAR NOT NULL,
    tenant_id VARCHAR(14) NOT NULL,
    region VARCHAR
 );
 
 CREATE TABLE MonMetrics.Dimensions(
-    metric_definition_id VARCHAR(20) NOT NULL,
+    metric_definition_id BINARY(20) NOT NULL,
     name VARCHAR NOT NULL,
     value VARCHAR NOT NULL,
     CONSTRAINT MetricsDimensionsConstraint UNIQUE(metric_definition_id, name, value)
 );
 
 CREATE TABLE MonMetrics.StagedDimensions(
-    metric_definition_id VARCHAR(20),
+    metric_definition_id BINARY(20),
     name VARCHAR NOT NULL,
     value VARCHAR NOT NULL
 );
